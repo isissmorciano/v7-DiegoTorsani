e25f80173afd1838cce0b36681da27c035af8331
diego <diego.torsani@studenti.isissgobetti.it>
2026-04-01 11:40:59 +0200

    o

--- ristorante/app/main.py
+++ ristorante/app/main.py
@@ -14,4 +14,17 @@ def index():
     categoria: list[dict] = categoria_repository.get_all_categories()
 
     # 2. Passiamo la variabile 'channels' al template
-    return render_template("base.html", categoria=categoria)
+    return render_template("index.html", categoria=categoria)
+
+@bp.route("/category/<int:id>")
+def category_detail(id):
+    # 1. Prendiamo il canale
+    category = categoria_repository.get_category_by_id(id)
+    if category is None:
+        abort(404, "categoria non trovata.")
+
+    # 2. Prendiamo i video del canale
+    piatti = piatto_repository.get_piatti_by_category(id)
+
+    # 3. Passiamo al template
+    return render_template("categoria_detail.html", category=category, piatto=piatti)
--- ristorante/app/repositories/piatto_repository.py
+++ ristorante/app/repositories/piatto_repository.py
@@ -2,3 +2,50 @@ from app.db import get_db
 
 
 # TODO: Implementa le funzioni richieste dall'esercizio 1 e 2
+def get_all_piatti():
+    """
+    Recupera tutti i canali.
+    """
+    db = get_db()
+    query = """
+        SELECT *
+        FROM piatti
+    """
+    channels = db.execute(query).fetchall()
+    return [dict(channel) for channel in channels]
+
+def get_piatti_by_category(category_id):
+    """Recupera un singolo canale per ID."""
+    db = get_db()
+    query = """
+        SELECT c.id, c.nome, c.numero_iscritti, p.nome AS categoria
+        FROM category c
+        JOIN piatti p ON p.categoria_id = c.id
+        WHERE c.id = ?
+    """
+    category = db.execute(query, (category_id,)).fetchone()
+    if category:
+        return dict(category)
+    return None
+
+def get_piatto_by_id(piatto_id):
+    """Recupera un singolo canale per ID."""
+    db = get_db()
+    query = """
+        SELECT *
+        FROM piatto p
+        WHERE p.id = ?
+    """
+    piatto = db.execute(query, (piatto_id,)).fetchone()
+    if piatto:
+        return dict(piatto)
+    return None
+
+def create_piatto(category_id, nome, prezzo):
+    """Crea un nuovo canale."""
+    db = get_db()
+    cursor = db.execute(
+        "INSERT INTO canali (category_id, nome, prezzo) VALUES (?, ?, ?)", (category_id, nome, prezzo)
+    )
+    db.commit()
+    return cursor.lastrowid
